6695581782f48b528919ebf9ace534f199a9cc6c
unknown <chaulagainsuyog@gmail.com>
2023-03-17 13:20:49 +0545

    first commit

---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,34 @@
+<script>
+    let step = 0;
+
+    let inputs = [
+        {
+            type: 'text',
+            name: 'Name'
+        },
+        {
+            type: 'number',
+            name: 'Phone Number'
+        },
+        {
+            type: 'email',
+            name: 'Email'
+        }
+    ]
+
+    function next(){
+        step += 1;
+    }
+ </script>
+
+<div>
+    {#each inputs as input, i}
+        {#if i <= step }
+            <label>{ input.name }</label>
+            <input type="{input.type}"/>
+            <br>
+        {/if}
+    {/each}
+    <button on:click={next}>Continue</button>
+
+</div>
